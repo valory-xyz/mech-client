8d0c8b3cdc3f262f3f99838869aa27a096fb5ccb
angrybayblade <vptl185@gmail.com>
2023-06-08 11:34:22 +0000

    chore: revert name changes

--- mech_client/cli.py
+++ mech_client/cli.py
@@ -24,19 +24,19 @@ from typing import Optional
 import click
 
 from mech_client import __version__
-from mech_client.interact import interact as interact
+from mech_client.interact import interact as interact_
 from mech_client.prompt_to_ipfs import main as prompt_to_ipfs_main
 from mech_client.push_to_ipfs import main as push_to_ipfs_main
 from mech_client.to_png import main as to_png_main
 
 
-@click.group(name="mechx")
+@click.group(name="mechx")  # type: ignore
 @click.version_option(__version__, prog_name="mechx")
 def cli() -> None:
     """Command-line tool for interacting with mechs."""
 
 
-@click.command(name="interact")
+@click.command()
 @click.argument("prompt")
 @click.argument("agent_id", type=int)
 @click.option(
@@ -47,12 +47,12 @@ def cli() -> None:
     "--key",
     type=click.Path(exists=True, file_okay=True, dir_okay=False),
 )
-def _interact(
+def interact(
     prompt: str, agent_id: int, tool: Optional[str], key: Optional[str]
 ) -> None:
     """Interact with a mech specifying a prompt and tool."""
     try:
-        interact(prompt=prompt, agent_id=agent_id, private_key_path=key, tool=tool)
+        interact_(prompt=prompt, agent_id=agent_id, private_key_path=key, tool=tool)
     except (ValueError, FileNotFoundError) as e:
         raise click.ClickException(str(e)) from e
 
@@ -60,14 +60,14 @@ def _interact(
 @click.command()
 @click.argument("prompt")
 @click.argument("tool")
-def _prompt_to_ipfs(prompt: str, tool: str) -> None:
+def prompt_to_ipfs(prompt: str, tool: str) -> None:
     """Upload a prompt and tool to IPFS as metadata."""
     prompt_to_ipfs_main(prompt=prompt, tool=tool)
 
 
 @click.command()
 @click.argument("file_path")
-def _push_to_ipfs(file_path: str) -> None:
+def push_to_ipfs(file_path: str) -> None:
     """Upload a file to IPFS."""
     push_to_ipfs_main(file_path=file_path)
 
@@ -76,15 +76,15 @@ def _push_to_ipfs(file_path: str) -> None:
 @click.argument("ipfs_hash")
 @click.argument("path")
 @click.argument("request_id")
-def _to_png(ipfs_hash: str, path: str, request_id: str) -> None:
+def to_png(ipfs_hash: str, path: str, request_id: str) -> None:
     """Convert a stability AI API's diffusion model output into a PNG format."""
     to_png_main(ipfs_hash, path, request_id)
 
 
-cli.add_command(_interact)
-cli.add_command(_prompt_to_ipfs)
-cli.add_command(_push_to_ipfs)
-cli.add_command(_to_png)
+cli.add_command(interact)
+cli.add_command(prompt_to_ipfs)
+cli.add_command(push_to_ipfs)
+cli.add_command(to_png)
 
 
 if __name__ == "__main__":
