e36472d095fa4c9120b0de18e8d780f7feac1323
angrybayblade <vptl185@gmail.com>
2023-06-08 06:42:20 +0000

    feat: support for fetching and selecting prompting tools dynamically

--- mech_client/interact.py
+++ mech_client/interact.py
@@ -29,18 +29,26 @@ import json
 import os
 import time
 import warnings
-from typing import Any, Dict, Optional
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Tuple
 
 import requests
 import websocket
 from aea.contracts.base import Contract
 from aea_ledger_ethereum import EthereumApi, EthereumCrypto
+from web3.contract import Contract as Web3Contract
 
 from mech_client.prompt_to_ipfs import push_metadata_to_ipfs
+from mech_client.subgraph import query_agent_address
 
-CONTRACT_ADDRESS = "0xFf82123dFB52ab75C417195c5fDB87630145ae81"
-ETHEREUM_TESTNET_CONFIG = {
-    "address": "https://rpc.eu-central-2.gateway.fm/v4/gnosis/non-archival/mainnet",
+
+AGENT_REGISTRY_CONTRACT = "0xE49CB081e8d96920C38aA7AB90cb0294ab4Bc8EA"
+MECHX_CHAIN_RPC = os.environ.get(
+    "MECHX_CHAIN_RPC",
+    "https://rpc.eu-central-2.gateway.fm/v4/gnosis/non-archival/mainnet",
+)
+LEDGER_CONFIG = {
+    "address": MECHX_CHAIN_RPC,
     "chain_id": 100,
     "poa_chain": False,
     "default_gas_price_strategy": "eip1559",
@@ -56,34 +64,87 @@ WSS_ENDPOINT = os.getenv(
     "WEBSOCKET_ENDPOINT",
     "wss://rpc.eu-central-2.gateway.fm/ws/v4/gnosis/non-archival/mainnet",
 )
+GNOSISSCAN_API_URL = "https://api.gnosisscan.io/api?module=contract&action=getabi&address={contract_address}"
 
 # Ignore a specific warning message
 warnings.filterwarnings("ignore", "The log with transaction hash.*")
 
+EthereumApi().api.eth.contract()
 
-def check_for_tools(tool: str) -> Optional[int]:
-    """Checks if the tool is valid and for what agent."""
-    # TODO - replace hardcoded logic with on-chain check against agent mech registry
-    return (
-        3
-        if tool
-        in [
-            "openai-text-davinci-002",
-            "openai-text-davinci-003",
-            "openai-gpt-3.5-turbo",
-            "openai-gpt-4",
-            "stabilityai-stable-diffusion-v1-5",
-            "stabilityai-stable-diffusion-xl-beta-v2-2-2",
-            "stabilityai-stable-diffusion-512-v2-1",
-            "stabilityai-stable-diffusion-768-v2-1",
-        ]
-        else None
+
+def get_contract(contract_address: str, ledger_api: EthereumApi) -> Web3Contract:
+    """Returns a contract instance."""
+    abi_request_url = GNOSISSCAN_API_URL.format(contract_address=contract_address)
+    response = requests.get(abi_request_url).json()
+    abi = json.loads(response["result"])
+    return ledger_api.get_contract_instance(
+        {"abi": abi, "bytecode": "0x"}, contract_address
     )
 
 
-def register_event_handlers(ethereum_crypto: EthereumCrypto) -> websocket.WebSocket:
+def _tool_selector_prompt(available_tools: List[str]) -> str:
+    """Tool selector prompt."""
+
+    tool_col_len = len(max(available_tools))
+    id_col_len = max(2, len(str(len(available_tools))))
+    table_len = tool_col_len + id_col_len + 5
+
+    separator = "|" + "-" * table_len + "|"
+    print("Select prompting tool")
+
+    def format_row(row: Tuple[str, str]) -> str:
+        _row = list(map(str, row))
+        row_str = "| "
+        row_str += _row[0]
+        row_str += " " * (id_col_len - len(_row[0]))
+        row_str += " | "
+        row_str += _row[1]
+        row_str += " " * (tool_col_len - len(_row[1]))
+        row_str += " |"
+        return row_str
+
+    while True:
+        print(separator)
+        print(format_row(("ID", "Tool")))
+        print(separator)
+        print("\n".join(map(format_row, enumerate(available_tools))))
+        print(separator)
+        try:
+            tool_id = int(input("Tool ID > "))
+            return available_tools[tool_id]
+        except (ValueError, TypeError, IndexError):
+            print("\nPlease enter valid tool ID.")
+
+
+def verify_or_retrieve_tool(
+    agent_id: int, ledger_api: EthereumApi, tool: Optional[str] = None
+) -> str:
+    """Checks if the tool is valid and for what agent."""
+    available_tools = fetch_tools(agent_id=agent_id, ledger_api=ledger_api)
+    if tool is not None and tool not in available_tools:
+        raise ValueError(
+            f"Provided tool `{tool}` not in the list of available tools; Available tools={available_tools}"
+        )
+    if tool is not None:
+        return tool
+    return _tool_selector_prompt(available_tools=available_tools)
+
+
+def fetch_tools(agent_id: int, ledger_api: EthereumApi) -> List[str]:
+    """Fetch tools for specified agent ID."""
+    mech_registry = get_contract(
+        contract_address=AGENT_REGISTRY_CONTRACT, ledger_api=ledger_api
+    )
+    token_uri = mech_registry.functions.tokenURI(agent_id).call()
+    response = requests.get(token_uri).json()
+    return response["tools"]
+
+
+def register_event_handlers(
+    wss: websocket.WebSocket, contract_address: str, crypto: EthereumCrypto
+) -> None:
     """Register event handlers."""
-    wss = websocket.create_connection(WSS_ENDPOINT)
+
     subscription_request = {
         "jsonrpc": "2.0",
         "id": 1,
@@ -91,82 +152,66 @@ def register_event_handlers(ethereum_crypto: EthereumCrypto) -> websocket.WebSoc
         "params": [
             "logs",
             {
-                "address": CONTRACT_ADDRESS,
+                "address": contract_address,
                 "topics": [
                     EVENT_SIGNATURE_REQUEST,
-                    ["0x" + "0" * 24 + ethereum_crypto.address[2:]],
+                    ["0x" + "0" * 24 + crypto.address[2:]],
                 ],
             },
         ],
     }
     content = bytes(json.dumps(subscription_request), "utf-8")
     wss.send(content)
+
     # registration confirmation
-    msg = wss.recv()
+    _ = wss.recv()
     subscription_deliver = {
         "jsonrpc": "2.0",
         "id": 1,
         "method": "eth_subscribe",
         "params": [
             "logs",
-            {"address": CONTRACT_ADDRESS, "topics": [EVENT_SIGNATURE_DELIVER]},
+            {"address": contract_address, "topics": [EVENT_SIGNATURE_DELIVER]},
         ],
     }
     content = bytes(json.dumps(subscription_deliver), "utf-8")
     wss.send(content)
+
     # registration confirmation
-    msg = wss.recv()
-    return wss
+    _ = wss.recv()
 
 
 def send_request(
-    ethereum_crypto: EthereumCrypto,
-    ethereum_ledger_api: EthereumApi,
+    crypto: EthereumCrypto,
+    ledger_api: EthereumApi,
+    mech_contract: Web3Contract,
     prompt: str,
     tool: str,
-    contract_address: str = CONTRACT_ADDRESS,
     price: int = 10_000_000_000_000_000,
-) -> Contract:
+) -> None:
     """Sends a request to the mech."""
-    mech = check_for_tools(tool)
-    if mech is None:
-        raise ValueError(f"Tool {tool} is not supported by any mech.")
     v1_file_hash_hex_truncated, v1_file_hash_hex = push_metadata_to_ipfs(prompt, tool)
     print(f"Prompt uploaded: https://gateway.autonolas.tech/ipfs/{v1_file_hash_hex}")
 
-    gnosisscan_api_url = f"https://api.gnosisscan.io/api?module=contract&action=getabi&address={contract_address}"
-    response = requests.get(gnosisscan_api_url)
-    abi = response.json()["result"]
-    abi = json.loads(abi)
-
-    contract_instance = ethereum_ledger_api.get_contract_instance(
-        {"abi": abi, "bytecode": "0x"}, contract_address
-    )
     method_name = "request"
-    methord_args = {
-        "data": v1_file_hash_hex_truncated
-    }  # bytes.fromhex(v1_file_hash_hex_truncated[2:])}
-    tx_args = {"sender_address": ethereum_crypto.address, "value": price}
-
-    raw_transaction = ethereum_ledger_api.build_transaction(
-        contract_instance=contract_instance,
+    methord_args = {"data": v1_file_hash_hex_truncated}
+    tx_args = {"sender_address": crypto.address, "value": price}
+    raw_transaction = ledger_api.build_transaction(
+        contract_instance=mech_contract,
         method_name=method_name,
         method_args=methord_args,
         tx_args=tx_args,
     )
     raw_transaction["gas"] = 50_000
-    # raw_transaction = ethereum_ledger_api.update_with_gas_estimate(raw_transaction)
-    signed_transaction = ethereum_crypto.sign_transaction(raw_transaction)
-    transaction_digest = ethereum_ledger_api.send_signed_transaction(signed_transaction)
+    signed_transaction = crypto.sign_transaction(raw_transaction)
+    transaction_digest = ledger_api.send_signed_transaction(signed_transaction)
     print(f"Transaction sent: https://gnosisscan.io/tx/{transaction_digest}")
-    return contract_instance
 
 
 def watch_for_events(
     wss: websocket.WebSocket,
-    contract_instance: Contract,
-    ethereum_ledger_api: EthereumApi,
-    ethereum_crypto: EthereumCrypto,
+    mech_contract: Web3Contract,
+    ledger_api: EthereumApi,
 ) -> Dict[str, Any]:
     """Watches for events on mech."""
     is_waiting = True
@@ -178,19 +223,17 @@ def watch_for_events(
         no_receipt = True
         while no_receipt:
             try:
-                tx_receipt = ethereum_ledger_api._api.eth.get_transaction_receipt(
-                    tx_hash
-                )
+                tx_receipt = ledger_api._api.eth.get_transaction_receipt(tx_hash)
                 no_receipt = False
             except Exception:
                 time.sleep(1)
         event_signature = tx_receipt["logs"][0]["topics"][0].hex()
         if event_signature == EVENT_SIGNATURE_REQUEST:
-            rich_logs = contract_instance.events.Request().processReceipt(tx_receipt)
+            rich_logs = mech_contract.events.Request().processReceipt(tx_receipt)
             request_id = rich_logs[0]["args"]["requestId"]
             print(f"Request on-chain with id: {request_id}")
         if event_signature == EVENT_SIGNATURE_DELIVER:
-            rich_logs = contract_instance.events.Deliver().processReceipt(tx_receipt)
+            rich_logs = mech_contract.events.Deliver().processReceipt(tx_receipt)
             data = rich_logs[0]["args"]["data"]
             request_id_ = rich_logs[0]["args"]["requestId"]
             if request_id != request_id_:
@@ -198,6 +241,7 @@ def watch_for_events(
             data_url = "https://gateway.autonolas.tech/ipfs/f01701220" + data.hex()
             print(f"Data arrived: {data_url}")
             is_waiting = False
+
     response = requests.get(data_url + "/" + str(request_id))
     response_json = response.json()
     result = response_json["result"]
@@ -205,12 +249,38 @@ def watch_for_events(
     return response_json
 
 
-def interact(prompt: str, tool: str) -> Dict[str, Any]:
-    ethereum_crypto = EthereumCrypto(private_key_path=PRIVATE_KEY_FILE_PATH)
-    ethereum_ledger_api = EthereumApi(**ETHEREUM_TESTNET_CONFIG)
-    wss = register_event_handlers(ethereum_crypto)
-    contract_instance = send_request(ethereum_crypto, ethereum_ledger_api, prompt, tool)
-    response = watch_for_events(
-        wss, contract_instance, ethereum_ledger_api, ethereum_crypto
+def interact(
+    prompt: str,
+    agent_id: int,
+    tool: Optional[str] = None,
+    private_key_path: Optional[str] = None,
+) -> Dict[str, Any]:
+    """Interact with agent mech contract."""
+    contract_address = query_agent_address(agent_id=agent_id)
+    if contract_address is None:
+        raise ValueError(f"Agent with ID {agent_id} does not exist!")
+
+    private_key_path = private_key_path or PRIVATE_KEY_FILE_PATH
+    if not Path(private_key_path).exists():
+        raise FileNotFoundError(
+            f"Private key file `{private_key_path}` does not exist!"
+        )
+
+    wss = websocket.create_connection(WSS_ENDPOINT)
+    crypto = EthereumCrypto(private_key_path=private_key_path)
+    ledger_api = EthereumApi(**LEDGER_CONFIG)
+
+    tool = verify_or_retrieve_tool(agent_id=agent_id, ledger_api=ledger_api, tool=tool)
+    mech_contract = get_contract(
+        contract_address=contract_address, ledger_api=ledger_api
     )
-    return response
+
+    register_event_handlers(wss=wss, contract_address=contract_address, crypto=crypto)
+    send_request(
+        crypto=crypto,
+        ledger_api=ledger_api,
+        mech_contract=mech_contract,
+        prompt=prompt,
+        tool=tool,
+    )
+    return watch_for_events(wss=wss, mech_contract=mech_contract, ledger_api=ledger_api)
