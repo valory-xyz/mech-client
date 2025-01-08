b2b8ec8d21103ae0b90526fd2cf4b5a97ec7ffc6
KahanMajmudar <kahan.m.btechi16@ahduni.edu.in>
2025-01-08 13:00:26 +0000

    adds the deposit and withdraw scripts for native as well as token mech request

---
+++ scripts/deposit_native.py
@@ -0,0 +1,106 @@
+import sys
+from pathlib import Path
+from aea_ledger_ethereum import EthereumApi, EthereumCrypto
+from dataclasses import asdict
+
+from mech_client.interact import get_mech_config, PRIVATE_KEY_FILE_PATH
+from utils import (
+    print_title,
+    input_select_chain,
+    input_with_default_value,
+    CHAIN_TO_NATIVE_BALANCE_TRACKER,
+)
+from mech_client.wss import wait_for_receipt
+
+
+def deposit(
+    ledger_api: EthereumApi,
+    crypto: EthereumCrypto,
+    to: str,
+    amount: int,
+):
+    sender = crypto.address
+
+    try:
+        print("Fetching user balance")
+        user_balance = ledger_api.get_balance(address=sender)
+        if user_balance < amount:
+            formatted_user_balance = user_balance / 1e18
+            formatted_amount = amount / 1e18
+            print("User balance low!!")
+            print(f"Balance: {formatted_user_balance}")
+            print(f"Want to Deposit: {formatted_amount}")
+            sys.exit(1)
+    except Exception as e:
+        print(f"Error occured while fetching user balance: {e}")
+
+    try:
+        print("Sending deposit tx")
+        print(f"{amount=}")
+        raw_transaction = ledger_api.get_transfer_transaction(
+            sender_address=sender,
+            destination_address=to,
+            amount=amount,
+            tx_fee=50000,
+            tx_nonce="0x",
+        )
+        signed_transaction = crypto.sign_transaction(raw_transaction)
+        transaction_digest = ledger_api.send_signed_transaction(
+            signed_transaction,
+            raise_on_try=True,
+        )
+        return transaction_digest
+    except Exception as e:  # pylint: disable=broad-except
+        print(f"Error occured while sending the transaction: {e}")
+
+
+def main() -> None:
+    """Runs the deposit functionality for the native mech type"""
+    print_title("Native Deposit")
+    print("This script will assist you in depositing native balance for mech requests.")
+    print()
+
+    print("Select the chain to use")
+
+    chain_config = input_select_chain()
+
+    amount = input_with_default_value(
+        "Please provide the amount of native currency to deposit", "1"
+    )
+    amount_to_deposit = int(float(amount) * 10**18)
+
+    private_key_path = input_with_default_value(
+        "Please provide path to your private key", PRIVATE_KEY_FILE_PATH
+    )
+
+    mech_config = get_mech_config(chain_config)
+    ledger_config = mech_config.ledger_config
+    ledger_api = EthereumApi(**asdict(ledger_config))
+
+    if not Path(private_key_path).exists():
+        raise FileNotFoundError(
+            f"Private key file `{private_key_path}` does not exist!"
+        )
+    crypto = EthereumCrypto(private_key_path=private_key_path)
+
+    chain_id = mech_config.ledger_config.chain_id
+    to = CHAIN_TO_NATIVE_BALANCE_TRACKER[chain_id]
+
+    deposit_tx = deposit(ledger_api, crypto, to, amount_to_deposit)
+    if not deposit_tx:
+        print("Unable to deposit")
+        sys.exit(1)
+
+    transaction_url_formatted = mech_config.transaction_url.format(
+        transaction_digest=deposit_tx
+    )
+    print(f" - Transaction sent: {transaction_url_formatted}")
+    print(" - Waiting for transaction receipt...")
+    wait_for_receipt(deposit_tx, ledger_api)
+
+    print("")
+    print("Deposit Successful")
+
+
+if __name__ == "__main__":
+    main()
