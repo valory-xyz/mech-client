8ac05ddbd1b023127556a827f2f8748c629679ba
Keshav Mishra <keshav.mishra0298@gmail.com>
2025-03-21 21:36:42 +0530

    update: supported network configs

--- scripts/nvm_subscription/manager.py
+++ scripts/nvm_subscription/manager.py
@@ -1,24 +1,40 @@
 # subscription/manager.py
+from enum import Enum
 import json
 import logging
 import os
+import sys
 
 import uuid
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Literal, Union
 from web3 import Web3
 from eth_account import Account
 from eth_typing import ChecksumAddress
 
-from config import NVMMechConfig
 from .contracts.did_registry import DIDRegistryContract
 from .contracts.nft_sales import NFTSalesTemplateContract
 from .contracts.lock_payment import LockPaymentConditionContract
 from .contracts.transfer_nft import TransferNFTConditionContract
 from .contracts.escrow_payment import EscrowPaymentConditionContract
+from .contracts.agreement_manager import AgreementStorageManagerContract
 
 logger = logging.getLogger(__name__)
 
-CONFIGS = json.load(open('scripts/nvm_subscription/resources/networks.json', 'r'))
+CONFIGS = json.load(open('nvm_mech_integration/resources/networks.json', 'r'))
+
+class Network(Enum):
+    GNOSIS = "GNOSIS"
+    BASE = "BASE"
+
+
+def get_variable_value(variable)->str:
+    try:
+        var = os.getenv(variable)
+        if var is None:
+            raise ValueError(f"Environment variable {variable} not found")
+        return var
+    except Exception as e:
+        raise e
 
 
 class NVMSubscriptionManager:
@@ -27,12 +43,12 @@ class NVMSubscriptionManager:
     using a series of smart contracts.
     """
 
-    def __init__(self):
+    def __init__(self, network: str):
         """
         Initialize the SubscriptionManager, including contract instances
         and Web3 connection.
         """
-        self.url = CONFIGS.GNOSIS['web3_provider_uri']
+        self.url = CONFIGS[network]["nvm"]['web3ProviderUri']
         self.web3 = Web3(Web3.HTTPProvider(self.url))
 
         self.account = Account.from_key(os.getenv("PVT_KEY"))
@@ -44,8 +60,10 @@ class NVMSubscriptionManager:
         self.transfer_nft = TransferNFTConditionContract(self.web3)
         self.escrow_payment = EscrowPaymentConditionContract(self.web3)
 
-        self.token_address = self.web3.to_checksum_address(os.getenv("TOKEN_ADDRESS"))
-        self.subscription_nft_address = self.web3.to_checksum_address(os.getenv("SUBSCRIPTION_NFT_ADDRESS"))
+        self.agreement_storage_manager = AgreementStorageManagerContract(self.web3)
+
+        self.token_address = self.web3.to_checksum_address(get_variable_value("TOKEN_ADDRESS"))
+        self.subscription_nft_address = self.web3.to_checksum_address(get_variable_value("SUBSCRIPTION_NFT_ADDRESS"))
         self.subscription_credits = int(os.getenv("SUBSCRIPTION_CREDITS", "1"))
         self.amounts = [int(os.getenv("PLAN_FEE_NVM", "0")), int(os.getenv("PLAN_PRICE_MECHS", "0"))]
 
@@ -70,33 +88,38 @@ class NVMSubscriptionManager:
         """
         logger.info(f"Creating subscription for DID: {did}")
 
+        did = did.replace("did:nv:", "0x")
+
         ddo = self.did_registry.get_ddo(did)
         service = next((s for s in ddo.get("service", []) if s.get("type") == "nft-sales"), None)
         if not service:
             logger.error("No nft-sales service found in DDO")
             return {"status": "error", "message": "No nft-sales service in DDO"}
 
+
+        self.publisher = service["templateId"]
+
         conditions = service["attributes"]["serviceAgreementTemplate"]["conditions"]
-        reward_address = conditions[0]["parameters"][1]["value"]
+        reward_address = self.escrow_payment.address
         receivers = conditions[0]["parameters"][-1]["value"]
 
         agreement_id_seed = self._generate_agreement_id_seed()
-        agreement_id = self.web3.solidity_keccak(['bytes32', 'address'], [agreement_id_seed.encode(), self.sender])
+        agreement_id = self.agreement_storage_manager.agreement_id(agreement_id_seed, self.sender)
 
         # Condition hashes
         lock_hash = self.lock_payment.hash_values(did, reward_address, self.token_address, self.amounts, receivers)
-        lock_id = self.lock_payment.generate_id(agreement_id.hex(), lock_hash)
+        lock_id = self.lock_payment.generate_id(agreement_id, lock_hash)
 
         transfer_hash = self.transfer_nft.hash_values(
             did,
-            self.sender,
+            ddo["proof"]["creator"],
             self.sender,
             self.subscription_credits,
             lock_id,
             self.subscription_nft_address,
             False
         )
-        transfer_id = self.transfer_nft.generate_id(agreement_id.hex(), transfer_hash)
+        transfer_id = self.transfer_nft.generate_id(agreement_id, transfer_hash)
 
         escrow_hash = self.escrow_payment.hash_values(
             did,
@@ -108,7 +131,7 @@ class NVMSubscriptionManager:
             lock_id,
             transfer_id
         )
-        escrow_id = self.escrow_payment.generate_id(agreement_id.hex(), escrow_hash)
+        escrow_id = self.escrow_payment.generate_id(agreement_id, escrow_hash)
 
         # Build transaction
         tx = self.nft_sales.build_create_agreement_tx(
@@ -128,7 +151,7 @@ class NVMSubscriptionManager:
         )
 
         signed_tx = self.web3.eth.account.sign_transaction(tx, private_key=os.getenv("PVT_KEY"))
-        tx_hash = self.web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        tx_hash = self.web3.eth.send_raw_transaction(signed_tx.raw_transaction)
         receipt = self.web3.eth.wait_for_transaction_receipt(tx_hash)
 
         if receipt["status"] == 1:
