926ffdecae92ce9be4732aa1793408ba99dd68df
KahanMajmudar <kahan.m.btechi16@ahduni.edu.in>
2025-05-15 12:15:05 +0000

    convert to dict

--- mech_client/subgraph.py
+++ mech_client/subgraph.py
@@ -48,6 +48,13 @@ DELIVER_QUERY_TEMPLATE = Template(
 """
 )
 DEFAULT_TIMEOUT = 600.0
+GNOSIS_AGENT_ID_TO_MECH_ADDRESSES = {
+    3: "0xFf82123dFB52ab75C417195c5fDB87630145ae81",
+    6: "0x77af31De935740567Cf4fF1986D04B2c964A786a",
+    9: "0x552cea7bc33cbbeb9f1d90c1d11d2c6daeffd053",
+    11: "0x9aDe7A78A39B39a44b7a084923E93AA0B19Fd690",
+    19: "0x45b73d649c7b982548d5a6dd3d35e1c5c48997d0",
+}
 
 
 def query_agent_address(  # pylint: disable=too-many-return-statements
@@ -70,16 +77,8 @@ def query_agent_address(  # pylint: disable=too-many-return-statements
     :rtype: Optional[str]
     """
     # temporary hard coded until subgraph present
-    if chain_config == "gnosis" and agent_id == 3:
-        return "0xFf82123dFB52ab75C417195c5fDB87630145ae81"
-    if chain_config == "gnosis" and agent_id == 6:
-        return "0x77af31De935740567Cf4fF1986D04B2c964A786a"
-    if chain_config == "gnosis" and agent_id == 9:
-        return "0x552cea7bc33cbbeb9f1d90c1d11d2c6daeffd053"
-    if chain_config == "gnosis" and agent_id == 11:
-        return "0x9aDe7A78A39B39a44b7a084923E93AA0B19Fd690"
-    if chain_config == "gnosis" and agent_id == 19:
-        return "0x45b73d649c7b982548d5a6dd3d35e1c5c48997d0"
+    if chain_config == "gnosis":
+        return GNOSIS_AGENT_ID_TO_MECH_ADDRESSES.get(agent_id, None)
     if chain_config == "base" and agent_id == 1:
         return "0x37C484cc34408d0F827DB4d7B6e54b8837Bf8BDA"
     if chain_config == "base" and agent_id == 2:
