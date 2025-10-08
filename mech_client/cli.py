9496c506fbc972c143d50a31c7cdc841c6922af5
KahanMajmudar <kahan.m.btechi16@ahduni.edu.in>
2025-10-08 13:35:20 +0000

    fix: correct cmd

--- mech_client/cli.py
+++ mech_client/cli.py
@@ -173,7 +173,7 @@ def cli(ctx: click.Context, client_mode: bool) -> None:
         operate_path = get_operate_path()
         if not operate_path.exists():
             raise ClickException(
-                f"""Operate path doesnot exists at: {operate_path}. Setup agent mode using mechx setup-agent-mode."""
+                f"""Operate path doesnot exists at: {operate_path}. Setup agent mode using mechx --client-mode setup-agent-mode."""
             )
 
 
